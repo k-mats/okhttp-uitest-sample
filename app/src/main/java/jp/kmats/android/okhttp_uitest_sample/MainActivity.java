1f43830e522fb7aab3400f678691e44b180b625f
k-mats <gfeenhaftsylphid@gmail.com>
2016-10-08 14:42:47 +0900

    Access example.com via OkHttp after pushing button

--- app/src/main/java/jp/kmats/android/okhttp_uitest_sample/MainActivity.java
+++ app/src/main/java/jp/kmats/android/okhttp_uitest_sample/MainActivity.java
@@ -2,10 +2,20 @@ package jp.kmats.android.okhttp_uitest_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,26 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                access("http://example.com/");
+            }
+        });
+    }
 
+    private void access(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        OkHttpClientHolder.getClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG, "failure");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "success");
             }
         });
     }
