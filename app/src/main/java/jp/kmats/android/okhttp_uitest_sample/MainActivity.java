c24135b40654df4e6ad2fea289a980810404ccd5
k-mats <gfeenhaftsylphid@gmail.com>
2016-10-08 14:34:09 +0900

    Add button to start OkHttp

--- app/src/main/java/jp/kmats/android/okhttp_uitest_sample/MainActivity.java
+++ app/src/main/java/jp/kmats/android/okhttp_uitest_sample/MainActivity.java
@@ -2,6 +2,8 @@ package jp.kmats.android.okhttp_uitest_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
